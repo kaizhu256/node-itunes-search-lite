2f132e49a2fe55ad0da6223770c051cdaa6d44f1
Travis CI User <travis@example.org>
2017-10-02 00:37:40 +0000

    [ci skip] update gh-pages

---
+++ build..alpha..travis-ci.org/example.sh
@@ -0,0 +1,10 @@
+# example.sh
+
+# this shell script will download and run a web demo of itunes-search-lite as a standalone app
+
+# 1. download standalone app
+curl -O https://kaizhu256.github.io/node-itunes-search-lite/build..beta..travis-ci.org/app/assets.app.js
+# 2. run standalone app
+node ./assets.app.js
+# 3. open a browser to http://127.0.0.1:8081 and play with the web demo
+# 4. edit file assets.app.js to suit your needs
